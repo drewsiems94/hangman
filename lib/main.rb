e0986bac5fe3a1e34c0064e833885842f030eaaa
Andrew Siems <siemsdrew@gmail.com>
2022-09-15 13:24:07 -0700

    Add instance variable to store player guesses

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,19 @@
+# frozen_string_literal: true
+
 require_relative 'computer'
 require_relative 'guess'
 require_relative 'game'
 require 'yaml'
 
+<<~INTRO
+
+  Let's play Hangman!
+
+  In this version, the computer will randomly select a word containing 5 to 12 letter.
+  You as the player will then have 8 chances to solve the word.
+  Note: you will not be penalized (i.e. lose a guess) for selecting a letter already chosen.
+
+INTRO
+
 code = Computer.new
-player = Guess.new
-Game.new(code.code, player)
+Game.new(code.code)
